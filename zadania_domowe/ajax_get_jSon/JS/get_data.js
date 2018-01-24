e7d5f7562e7c9c1abc5fcbdfd98174bc60be08d7
unknown <urszula giewon@gmail.com>
2018-01-24 18:53:45 +0100

    dodanie treści w pojawiającym się nowym divie z klasą #dane programisty

--- zadania_domowe/ajax_get_jSon/JS/get_data.js
+++ zadania_domowe/ajax_get_jSon/JS/get_data.js
@@ -7,12 +7,24 @@
                 $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function (data) {
 
 
-                    $("#dane-programisty").html("Imię: " + data.imie + "<br>" + "Nazwisko: " + data.nazwisko + "<br>" + "Zawód: " + data.zawod + "<br>" + "Firma: " + data.firma);
+                    var nowyKontener = $('<div/>', {
+                        id: 'dane-programisty',
+                        css: {
+                            width: "25vw",
+                            height: "auto",
+                            padding: "2vw"
+                        }
+                    });
+
+                    $(nowyKontener).html("Imię: " + data.imie + "<br>" + "Nazwisko: " + data.nazwisko + "<br>" + "Zawód: " + data.zawod + "<br>" + "Firma: " + data.firma);
+
+                    $("body").prepend(nowyKontener);
 
                 })
 
             }
         })
-
-
+        $("#button").onclick(function () {
+            "console.log(’test’)"
+        });
     });
