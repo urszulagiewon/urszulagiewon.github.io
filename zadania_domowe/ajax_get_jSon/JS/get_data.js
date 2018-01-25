8e556002f4623dfae4dc91cc8fa50c18dbccbff0
unknown <urszula giewon@gmail.com>
2018-01-25 17:50:28 +0100

    Kontener pojawiający się za przyciskiem

--- zadania_domowe/ajax_get_jSon/JS/get_data.js
+++ zadania_domowe/ajax_get_jSon/JS/get_data.js
@@ -18,7 +18,7 @@
 
                     $(nowyKontener).html("Imię: " + data.imie + "<br>" + "Nazwisko: " + data.nazwisko + "<br>" + "Zawód: " + data.zawod + "<br>" + "Firma: " + data.firma);
 
-                    $("body").prepend(nowyKontener);
+                    $("body").append(nowyKontener);
 
                 })
 
