21b7fb74d75217c7d26c44d32e576964d014708c
unknown <urszula giewon@gmail.com>
2018-01-26 21:37:48 +0100

    Dodanie funkcji on click

--- zadania_domowe/kurs_bitcoina/js/check_value.js
+++ zadania_domowe/kurs_bitcoina/js/check_value.js
@@ -1,42 +1,52 @@
 'use strict';
 $(function () {
 
+    $("#sprawdz-kurs").on({
+        "click": function () {
 
-    $.ajax({
-        url: "https://blockchain.info/pl/ticker?callback=callbackFnc&_=1516902571140",
-        type: 'get',
-        dataType: "json",
-        asnync: "true",
-        cache: "true",
-        success: function (data) {
-            var kursBitcoina = JSON.stringify(data);
-            var poziomPierwszy = JSON.parse(kursBitcoina);
-            var kursyBitcoina = poziomPierwszy;
-            var polskiKurs = kursyBitcoina.PLN;
-            var ostatniKurs = kursyBitcoina.PLN.last;
-            var sprzedaz = kursyBitcoina.PLN.sell;
-            var kupno = kursyBitcoina.PLN.buy;
-            var symbol = kursyBitcoina.PLN.symbol;
-   
-            
-           $("#kupno").html(kupno + " " + symbol);
-            if (kupno < ostatniKurs){ 
-                $("#kupno").append(" w dół").css({"color":"red"});
-            } else {
-                $("#kupno").append(" w górę ").css({"color":"green"});
-            }
-            
-            $("#sprzedaz").html(sprzedaz + " " + symbol);
-            if (sprzedaz < ostatniKurs){ 
-                $("#sprzedaz").append(" w dół").css({"color":"red"});
-            } else {
-                $("#sprzedaz").append(" w górę ").css({"color":"green"});
-            }
-            
-            
-            console.log("Kurs bitcoina dla Polski to: " + ostatniKurs + symbol + " sprzedaż " + sprzedaz + symbol + " kupno " + kupno + symbol);
-        },
-    });
-    
-    
+            $.ajax({
+                url: "https://blockchain.info/pl/ticker?callback=callbackFnc&_=1516902571140",
+                type: 'get',
+                dataType: "json",
+                asnync: "true",
+                cache: "true",
+                success: function (data) {
+                    var kursBitcoina = JSON.stringify(data);
+                    var poziomPierwszy = JSON.parse(kursBitcoina);
+                    var kursyBitcoina = poziomPierwszy;
+                    var polskiKurs = kursyBitcoina.PLN;
+                    var ostatniKurs = kursyBitcoina.PLN.last;
+                    var sprzedaz = kursyBitcoina.PLN.sell;
+                    var kupno = kursyBitcoina.PLN.buy;
+                    var symbol = kursyBitcoina.PLN.symbol;
+
+
+                    $("#kupno").html(kupno + " " + symbol);
+                    if (kupno < ostatniKurs) {
+                        $("#kupno").append(" w dół").css({
+                            "color": "red"
+                        });
+                    } else {
+                        $("#kupno").append(" w górę ").css({
+                            "color": "green"
+                        });
+                    }
+
+                    $("#sprzedaz").html(sprzedaz + " " + symbol);
+                    if (sprzedaz < ostatniKurs) {
+                        $("#sprzedaz").append(" w dół").css({
+                            "color": "red"
+                        });
+                    } else {
+                        $("#sprzedaz").append(" w górę ").css({
+                            "color": "green"
+                        });
+                    }
+
+
+                    console.log("Kurs bitcoina dla Polski to: " + ostatniKurs + symbol + " sprzedaż " + sprzedaz + symbol + " kupno " + kupno + symbol);
+                },
+            });
+        }
+    })
 });
