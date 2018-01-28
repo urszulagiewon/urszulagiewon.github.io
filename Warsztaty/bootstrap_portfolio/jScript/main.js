a0dc10b0139511ad4b738a1cfee05eb13836dd52
unknown <urszula giewon@gmail.com>
2018-01-28 19:59:57 +0100

    Praca nad sekcją umiejętności

--- Warsztaty/bootstrap_portfolio/jScript/main.js
+++ Warsztaty/bootstrap_portfolio/jScript/main.js
@@ -24,4 +24,56 @@
             })
         })
 
+
+
+        $("#ind").on({
+            "click": function () {
+                $("#indesign").animate({
+                    "backgroundColor": "#A0D329",
+                    "width": "100%",
+                    "height": "1em"
+                }, 3000, function(){
+                    $("#ind").append("<p>Indesign na 100%!</p>").css({"padding":"0.5em"});
+                });
+            }
+        });
+
+        $("#ps").on({
+            "click": function () {
+                $("#photoshop").animate({
+                    "backgroundColor": "#A0D329",
+                    "width": "100%",
+                    "height": "1em"
+                }, 3000, function(){
+                    $("#ps").append("<p>Photoshop na 100%!</p>").css({"padding-top":"0.5em"});
+                });
+            }
+        });
+
+
+        $("#il").on({
+            "click": function () {
+                $("#illustrator").animate({
+                    "backgroundColor": "#A0D329",
+                    "width": "100%",
+                    "height": "1em"
+                }, 3000, function(){
+                    $("#il").append("<p>Illustrator na 100%!</p>").css({"padding-top":"0.5em"});
+                });
+            }
+        });
+
+        $("#cor").on({
+            "click": function () {
+                $("#corel").animate({
+                    "backgroundColor": "#DB9E1D",
+                    "width": "50%",
+                    "height": "1em"
+                }, 3000, function(){
+                    $("#cor").append("<p>Corel na 50%, ale mogę się nauczyć ;)</p>").css({"padding-top":"0.5em"});
+                });
+            }
+        });
+
+
     });
